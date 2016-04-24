036a3f902ca4a77662899b3a8fa766575bdbe39f
Watch1k <host.sf@gmail.com>
2016-04-25 00:31:59 +0300

    Initial commit

---
+++ build/js/app.js
@@ -0,0 +1,7 @@
+head.load(
+	// "js/lib/jquery.min.js",
+	// "js/lib/slick.min.js",
+	// "js/lib/wow.min.js",
+	// "js/lib/owl.carousel.min.js",
+	"js/common.js"
+);
---
+++ src/js/common.js
@@ -0,0 +1,16 @@
+head.ready(function(){
+
+	// Clear placeholder
+	(function() {
+		$('input,textarea').focus(function(){
+				$(this).data('placeholder',$(this).attr('placeholder'))
+				$(this).attr('placeholder','');
+		});
+		$('input,textarea').blur(function(){
+			$(this).attr('placeholder',$(this).data('placeholder'));
+		});
+	}());
+
+	// new WOW().init();
+	
+});
